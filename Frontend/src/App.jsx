d086212695499e82a7d190bc052eae38d51451fe
Nourseen Saeed <nourseensaeed7@gmail.com>
2026-03-12 01:32:49 +0200

    app Changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import {
-  BrowserRouter,
+  HashRouter,
   Routes,
   Route,
-//   Navigate,
 } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Details from "./pages/Details.jsx";
 import Form from "./pages/Form.jsx";
 import Vans from "./pages/AllVans.jsx"
 import Rental from "./pages/rentInst.jsx"
+
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Vehicle-Details/:id" element={<Details/>}/>
-        <Route path="/Van-Form/:id"element={<Form />}/>
+        <Route path="/Van-Form/:id" element={<Form />}/>
         <Route path="/vans" element={<Vans />} />
         <Route path="/Rental-Instructions" element={<Rental />} />
-
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
+
 export default App;
