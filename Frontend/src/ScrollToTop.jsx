c818aec841aa215b512ad84d6cc61c38af9f9459
Nourseen Saeed <nourseensaeed7@gmail.com>
2026-03-12 02:02:12 +0200

    scroll

--- Frontend/src/ScrollToTop.jsx
+++ Frontend/src/ScrollToTop.jsx
@@ -2,11 +2,11 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname, hash]);
 
   return null;
 };
